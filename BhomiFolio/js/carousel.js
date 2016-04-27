640a69b8a58486f6d74ec182f019092e74902059
Bhomi <bhomiboy@gmail.com>
2016-04-27 21:02:44 +1000

    Added files via upload

---
+++ BhomiFolio/js/carousel.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    
+    setInterval(function(){
+    
+        $('#carousel ul').animate({marginLeft:'-1000px'},2000,function(){            
+        
+            $(this).find("li:last").after($(this).find("li:first"));
+        
+            $(this).css({marginLeft:0});
+
+    });
+
+    },4000);    
+});
---
+++ BhomiFolio/js/recentwork.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.recentWorkBoxes').bxSlider({
+    slideWidth: 300,
+    minSlides: 2,
+    maxSlides: 2,
+    slideMargin: 10
+  });
+});
